ff7fd0af79dd46383fd69433d8e0fcab08e0aa6f
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-08 13:37:24 +1000

    Type definition test improvements

--- test/type-define.js
+++ test/type-define.js
@@ -4,7 +4,7 @@ var assert = require('assert'),
 
 describe('graph type definition', function() {
     before(function(done) {
-        graph = graphdb(require('./helpers/connect-opts'), done);
+        graph = graphdb(require('./helpers/connect-opts')).open(done);
     });
 
     it('should be able to define a new profile type', function() {
@@ -16,9 +16,16 @@ describe('graph type definition', function() {
 
     it('should be able to validate the type dictionary has a profile', function() {
         assert(graph.types.profile);
+    });
 
-        assert.deepEqual(graph.types.profile.id, { type: 'uuid' });
-        assert.deepEqual(graph.types.profile.name, { type: 'string' });
+    it('should have defined the id attribute of a profile as a uuid', function() {
+        assert(graph.types.profile.id);
+        assert.equal(graph.types.profile.id.type, 'uuid');
+    });
+
+    it('should have defined the name attribute of a profile as a string', function() {
+        assert(graph.types.profile.name);
+        assert.equal(graph.types.profile.name.type, 'string');
     });
 
     after(function(done) {
