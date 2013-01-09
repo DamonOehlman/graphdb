b3c95c1945c87d9eba2a7f70f8807f237187d9e5
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-09 16:17:46 +1000

    orient connector coming along

--- test/helpers/connect-opts.js
+++ test/helpers/connect-opts.js
@@ -2,3 +2,20 @@ var _ = require('underscore'),
     opts = module.exports = _.defaults({
         connector: (process.env.GRAPHDB_CONNECTOR || '').toLowerCase() || undefined
     }, { connector: 'memory' });
+
+switch (opts.connector) {
+    case 'orient': {
+        opts.server = {
+            host: 'localhost',
+            port: 2424,
+            user_name: 'root',
+            user_password: process.env.ORIENTDB_ROOTPASS || 'AE825CAAEE7971AC797A62751A88CE813664CD3DB4979BFAB881C393A7F958FB'
+        };
+
+        opts.db = {
+            name: 'graphdb-test',
+            user_name: 'admin',
+            user_password: 'admin'
+        };
+    }
+}
