4d0ab8973ae48dcf54ff61d4868244951d1b6338
Damon Oehlman <damon.oehlman@sidelab.com>
2013-01-25 15:23:28 +1000

    Modified connect opts to use orienteer connection opts

--- test/helpers/connect-opts.js
+++ test/helpers/connect-opts.js
@@ -1,21 +1,28 @@
 var _ = require('underscore'),
     opts = module.exports = _.defaults({
+        db: 'graphdb-test',
         connector: (process.env.GRAPHDB_CONNECTOR || '').toLowerCase() || undefined
     }, { connector: 'memory' });
 
 switch (opts.connector) {
     case 'orient': {
-        opts.server = {
-            host: 'localhost',
-            port: 2424,
-            user_name: 'root',
-            user_password: process.env.ORIENTDB_ROOTPASS || 'AE825CAAEE7971AC797A62751A88CE813664CD3DB4979BFAB881C393A7F958FB'
+
+        opts.protocol = {
+            type:   'http',
+            host:   process.env.ORIENTDB_HOST || 'localhost',
+            port:   2480,
+            https:  false
         };
 
-        opts.db = {
-            name: 'graphdb-test',
-            user_name: 'admin',
-            user_password: 'admin'
+        opts.users = {
+            root:   process.env.ORIENTDB_ROOTPASS || 'FDB6C4ED52608E5942F84FE0CAE4C33462846C0B2F0FE0AAE5FA492BB72B6DDE',
+            admin:  'admin',
+            writer: 'writer',
+            reader: 'reader'
+        };
+
+        opts.dbs = {
+            'graphdb-test': {}
         };
     }
 }
